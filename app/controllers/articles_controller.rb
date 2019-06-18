63bef189d04e134c320979d3f8c8baa3a8902e8f
Vev <vev.gaunt@gmail.com>
2019-06-18 20:35:18 +0100

    created article 'create' and 'show' functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,24 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    # render plain: params[:article].inspect # inspect will display the created article -- this displays the hash
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice]="Article created"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 end
