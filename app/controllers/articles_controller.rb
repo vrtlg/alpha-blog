4f2b68bbfde97913d6931390a050f182695852c9
Vev <vev.gaunt@gmail.com>
2019-06-19 21:50:04 +0100

    created article edit and begun index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @all_articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -14,8 +19,22 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
   end
 
   private
