47ddcdc26e2bb1501266a3c0c50f23bc954c1206
Vev <vev.gaunt@gmail.com>
2019-07-10 20:10:56 +0100

    completed sign up and edit user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,33 @@ class UsersController < ApplicationController
     @user=User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice]="#{@user.username}, your signup was successful"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="User credentials updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'signup', to: 'users#new'
-  post 'users', to:'users#create'
+  resources :users, except: [:new]
 end
