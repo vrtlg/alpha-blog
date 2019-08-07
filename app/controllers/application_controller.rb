30a23e031835e07d9fa678378e4e0c0d2e272e35
Vev <vev.gaunt@gmail.com>
2019-08-07 16:49:01 +0100

    Model optimisation, nav bar edits with login authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in? #makes these methods available to views
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] # return current user (if it's already been found in the database) OR make user available if the user is logged in
+  end
+
+  def logged_in?
+    !!current_user #converts to boolean, will return true if logged in
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:notice] = "You must be logged in first"
+      redirect_to login_path
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action  :set_user, only: [:edit, :update, :show] #this replaces having the below code in each of these actions:
+      # @user = User.find(params[:id])
 
   def new
     @user=User.new
@@ -15,11 +17,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice]="User credentials updated"
       redirect_to articles_path
@@ -29,8 +29,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-
   end
 
   def index
@@ -40,6 +38,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
