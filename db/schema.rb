17201c734ee3a3b49abcdda74da1e8f1a30e2d5c
Vev <vev.gaunt@gmail.com>
2019-08-14 19:18:07 +0100

    many-to-many association between articles and categories implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_183250) do
+ActiveRecord::Schema.define(version: 2019_08_14_180613) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
