fd606027b2f5074ba632b2bc20e2a7887522b573
Vev <vev.gaunt@gmail.com>
2019-08-16 19:12:07 +0100

    gave admin edit power of categories, fixed positioning of footer

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :require_admin, only: [:new, :create]
+  before_action :require_admin, only: [:new, :create, :edit]
 
   def index
     @categories = Category.all
@@ -24,13 +24,27 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def category_params
     params.require(:category).permit(:name)
   end
 
   def require_admin
-    if !logged_in || (logged_in? and !current_user.admin?) #need to be logged in as admin to do this
+    if !logged_in? || (logged_in? and !current_user.admin?) #need to be logged in as admin to do this
       flash[:notice] = "You must be an administrator to perform that action"
       redirect_to categories_path
     end
